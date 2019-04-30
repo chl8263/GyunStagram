e653118420b0e2094790a533eea77225c13fc102
chl8263 <chl8263@naver.com>
2019-05-01 01:45:38 +0900

    addPhoto mvc 작업중

--- app/src/main/java/com/example/gyunstagram/view/MainActivity.kt
+++ app/src/main/java/com/example/gyunstagram/view/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.gyunstagram.view
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.gyunstagram.R
 import com.example.gyunstagram.core.BaseActivity
 import com.example.gyunstagram.databinding.ActivityMainBinding
-import com.example.gyunstagram.view.navigation.AlarmFragment
-import com.example.gyunstagram.view.navigation.DetailViewFragment
-import com.example.gyunstagram.view.navigation.GridFragment
-import com.example.gyunstagram.view.navigation.UserFragment
+import com.example.gyunstagram.view.navigation.*
 import com.example.gyunstagram.viewModel.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.jar.Manifest
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
@@ -25,6 +27,9 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         viewDataBinding.viewModel = viewModel
 
         viewModel.mNavigationItemSelectedListener = navigationSelectedListener
+
+        //permission setting and request first enter app
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
     override fun initDataBinding() {
@@ -52,6 +57,9 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
             }
             R.id.action_add_photo -> {
                 //replaceFragment(TwoFragment.newInstance())
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.action_favorite_alarm -> {
--- app/src/main/java/com/example/gyunstagram/view/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/gyunstagram/view/navigation/AddPhotoActivity.kt
@@ -1,17 +1,29 @@
 package com.example.gyunstagram.view.navigation
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil.setContentView
 import com.example.gyunstagram.R
 import com.example.gyunstagram.core.BaseActivity
 import com.example.gyunstagram.databinding.ActivityAddPhotoBinding
+import com.example.gyunstagram.util.toast
 import com.example.gyunstagram.viewModel.AddPthotoViewModel
 import com.example.gyunstagram.viewModel.LoginViewModel
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPhotoActivity : BaseActivity<ActivityAddPhotoBinding,AddPthotoViewModel>() {
 
+    var PICK_IMAGE_FROM_ALBUM = 0
+    lateinit var storage : FirebaseStorage
+    lateinit var photoUri : Uri
 
     override val layoutResourceId: Int
         get() = R.layout.activity_add_photo
@@ -19,7 +31,22 @@ class AddPhotoActivity : BaseActivity<ActivityAddPhotoBinding,AddPthotoViewModel
     override val viewModel : AddPthotoViewModel by viewModel()
 
     override fun initStartView() {
+
+        //initiate storage
+        storage = FirebaseStorage.getInstance()
+
+        //open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)
+
+        //add image upload event
+        photo_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+
     }
+
 
     override fun initDataBinding() {
     }
@@ -27,4 +54,31 @@ class AddPhotoActivity : BaseActivity<ActivityAddPhotoBinding,AddPthotoViewModel
     override fun initAfterBinding() {
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(resultCode == Activity.RESULT_OK){
+                // This is path to the selected image
+                photoUri = data?.data!!
+                addPhoto_image.setImageURI(photoUri)
+
+            }else {
+                //exit the addPhotoActivity if you leave album without selecting it
+                finish()
+            }
+        }
+    }
+    fun contentUpload(){
+        //Make fileName
+
+        var timestamp = SimpleDateFormat("yyyy.MMdd_HHmmss").format(Date())
+        var imageFIleName = "IMAGE_${timestamp}_.png"
+
+        var storageRef = storage.reference.child("images").child(imageFIleName)
+
+        //FileUpload
+        storageRef.putFile(photoUri).addOnSuccessListener {
+            applicationContext.toast(getString(R.string.upload_success))
+        }
+    }
 }
