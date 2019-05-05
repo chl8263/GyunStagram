742dd35270a47f8c15514ae32c127b937a716e96
chl8263 <chl8263@naver.com>
2019-05-05 22:17:54 +0900

    loading animation 작업 완료

--- app/src/main/java/com/example/gyunstagram/view/CustomProgressDialog.kt
+++ app/src/main/java/com/example/gyunstagram/view/CustomProgressDialog.kt
@@ -2,8 +2,26 @@ package com.example.gyunstagram.view
 
 import android.app.ProgressDialog
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Window
+import com.example.gyunstagram.R
 
 class CustomProgressDialog(context : Context) : ProgressDialog(context){
 
-    private
+    init {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+        setCanceledOnTouchOutside(false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress)
+
+        getWindow().setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+
+
+    }
 }
