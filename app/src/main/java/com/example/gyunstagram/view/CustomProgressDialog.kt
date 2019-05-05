052cecd8c715feb2f02c682dae333b1c2f8f7afa
chl8263 <chl8263@naver.com>
2019-05-05 22:13:50 +0900

    loading dialog 추가

---
+++ app/src/main/java/com/example/gyunstagram/view/CustomProgressDialog.kt
@@ -0,0 +1,9 @@
+package com.example.gyunstagram.view
+
+import android.app.ProgressDialog
+import android.content.Context
+
+class CustomProgressDialog(context : Context) : ProgressDialog(context){
+
+    private
+}
